2fdfeed93cb4eff22bfb5580b9421e16021ab101
leiyali <1140796384@qq.com>
2020-06-02 10:05:46 +0800

    master恢复

--- test_2020053001.py
+++ test_2020053001.py
@@ -18,9 +18,7 @@ cols = sheet.col_values(2)  # 获取第3列内容
 print(sheet.name, sheet.nrows, sheet.ncols)
 
 print('-------------------------->>>\n')
-print('--------------test bugfix_dev branch------------>>>\n')
-print('--------------test bugfix_dev branch------------>>>\n')
-print('--------------test bugfix_dev branch------------>>>\n')
+
 
 
 class RunMain:
