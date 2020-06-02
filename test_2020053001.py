106b881f782d9460f59eafb908226cd184c071ce
leiyali <1140796384@qq.com>
2020-06-02 09:58:01 +0800

    C4

--- test_2020053001.py
+++ test_2020053001.py
@@ -18,7 +18,7 @@ cols = sheet.col_values(2)  # 获取第3列内容
 print(sheet.name, sheet.nrows, sheet.ncols)
 
 print('-------------------------->>>\n')
-print('--------------33333------------>>>\n')
+print('--------------test dev branch------------>>>\n')
 
 
 class RunMain:
